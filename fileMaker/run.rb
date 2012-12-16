bb7b9c5c9b4b32e12f9972c0457365153b947186
Tomyail <tomyail1989@gmail.com>
2012-12-17 00:38:30 +0800

    complete filemaker

--- fileMaker/run.rb
+++ fileMaker/run.rb
@@ -17,7 +17,7 @@ end
 def readConfig()
 	file = File.new("struct.txt","r");
 	hash = {}
-	paretntHash = {}
+	paretntHash = {0=>0}
 	counter = 0
 	pre = 0
 	current = 0
@@ -26,54 +26,74 @@ def readConfig()
 		current = getTabNum(line)
 		if(!hash[current])
 			hash[current] = [];
-			paretntHash[current]
 		end
 		if(pre != current)
 			if(!paretntHash[current])
-				paretntHash[current] = 0
+				paretntHash[current] = hash[current-1].length-1
 			else
 				if(current - pre >0)
 					paretntHash[current]+=1
 				else
-					puts ""
 					paretntHash[current]
 				end
 				
 			end
 			pre = current
 		end
-		if(current == 0)
-			parent = 0
-		else
+		#if(current == 0)
+		#	parent = 0
+		#else
 			parent = paretntHash[current]
-		end
+		#end
 		line = line.gsub(/\s+/, "")
 		#puts "#{current},#{line},#{paretntHash[current]}"
 		hash[current].push({"value"=>line,"parent"=>parent});
 		counter = counter+1
 	end
 	file.close
-	#puts hash
 	#assume key has sorted
 	index = 0
 	for arr in hash.values
 		for obj in arr
-			getFullPath(hash,obj,index-1)
+			make(getFullPath(hash,obj,index-1))
 		end
 		index = index+1
+	end
+end
+
+def make(str)
+	result = []
+	arr = str.split("/").reverse
+	for i in arr
+		result.push(i)
+		break if isFile(i)
+	end
+	if(isFile(result[result.length-1]))
+		makeFile(getRealName(result))
+	else
+		makeFolder(getRealName(result))
 	end
 end
 
 def getFullPath(hash,value,index)
 	#deep = value.parent
 	path = value["value"]
-	puts path
 	index.downto(0) do |i|
-		#puts hash[i][value["parent"]]["value"]
+		path += "/"+hash[i][value["parent"]]["value"]
 		value = hash[i][value["parent"]]
-		puts i 
-		#value = hash[i][value.parent]
 	end
+	return path
+end
+
+def getRealName(arr)
+	return Dir::pwd+"/"+arr.join("/").gsub(/d:/,"").gsub(/f:/,"")
+end
+def isFile(value)
+	return value.split(":")[0] == "f"
+end
+
+def isDir(value)
+	return value.split(":")[0] == "d"
 end
 
 def getTabNum(str)
@@ -86,3 +106,8 @@ def getTabNum(str)
 end
 
 readConfig();
+
+#10.downto(1) do |i|
+#	break if i== 8
+#	puts i
+#end
